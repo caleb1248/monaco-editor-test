0e51ea082c55fd689be9a5bd2b554556c5927c78
caleb1248 <caleb@tremontschool.org>
2023-11-03 18:15:09 -0400

    I did some stuff

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,26 @@
 import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 self.MonacoEnvironment = {
-  getWorker: function (workerId: string, label: string) {
-    const getWorkerModule = (moduleUrl: string, label: string): Worker => {
-      // @ts-ignore
-      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
-        name: label,
-        type: 'module',
-      });
-    };
-
-    switch (label) {
-      case 'json':
-        return getWorkerModule(
-          '/monaco-editor/esm/vs/language/json/json.worker?worker',
-          label
-        );
-      case 'css':
-      case 'scss':
-      case 'less':
-        return getWorkerModule(
-          '/monaco-editor/esm/vs/language/css/css.worker?worker',
-          label
-        );
-      case 'html':
-      case 'handlebars':
-      case 'razor':
-        return getWorkerModule(
-          '/monaco-editor/esm/vs/language/html/html.worker?worker',
-          label
-        );
-      case 'typescript':
-      case 'javascript':
-        return getWorkerModule(
-          '/monaco-editor/esm/vs/language/typescript/ts.worker?worker',
-          label
-        );
-      default:
-        return getWorkerModule(
-          '/monaco-editor/esm/vs/editor/editor.worker?worker',
-          label
-        );
-    }
-  },
+  getWorker: function (_moduleId: any, label: string) {
+      if (label === 'json') {
+          return new jsonWorker();
+      }
+      if (label === 'css' || label === 'scss' || label === 'less') {
+          return new cssWorker();
+      }
+      if (label === 'html' || label === 'handlebars' || label === 'razor') {
+          return new htmlWorker();
+      }
+      if (label === 'typescript' || label === 'javascript') {
+          return new tsWorker();
+      }
+      return new editorWorker();
+  }
 };
 const editorDiv = document.body.appendChild(document.createElement('div'));
 
@@ -54,5 +32,6 @@ const model = monaco.editor.createModel(
 const editor = monaco.editor.create(editorDiv, {
   language: 'typescript',
   theme: 'vs-dark',
+  automaticLayout: true
 });
 editor.setModel(model);
